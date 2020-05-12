bde88dc8400fea24a221ecd0d5e23f7975953685
sravani <allenkisravani996@gmail.com>
2020-05-12 23:15:18 +0530

    Total wages

--- src/com/empapp/test/EmpApptest.java
+++ src/com/empapp/test/EmpApptest.java
@@ -6,27 +6,35 @@ import com.empapp.controller.EmpController;
  class EmpAppTest {
  
      public static void main(String[] args) {
+       int status=0;
+       int workhrs=0;
+       int dailywage=0;
+       int totalwage=0;
+       for(int days=0; days<20; days++){
 
-        Employee employee = new Employee();
-        EmpController empcontroller = new EmpController();
-        int status = empcontroller.EmployeeStatus();
-        switch( status )
-        {
-         case 0:
+               Employee employee = new Employee();
+               EmpController empcontroller = new EmpController();
+               status = empcontroller.EmployeeStatus();
+               switch( status )
+              {
+               case 0:
                  System.out.println("IsAbsent");
                  break;
-         case 1:
+               case 1:
                  System.out.println("IsPresent");
-                 int workhrs = empcontroller.Employee_Workhrs();
-                 switch( workhrs )
+                 workhrs = empcontroller.Employee_Workhrs();
+                 /*switch( workhrs )
                  {
                    case 8:
                            System.out.println("fulltime is : "+  empcontroller.EmployeeWage(workhrs));
                            break;
                    default:
                            System.out.println("Parttime is : "+  empcontroller.EmployeeWage(workhrs));
-                 }
+                  }*/
+                  totalwage+=empcontroller.EmployeeWage(workhrs);
          }
      }
+      System.out.println("MonthlyWage is: "+ totalwage);
 }
        
+}
